488f29a90f2013e3c4fa2b20d85b886938dbc4d1
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2022-03-12 21:41:18 +0100

    Building model with api keras

--- Malaria/Malaria.py
+++ Malaria/Malaria.py
@@ -50,5 +50,43 @@ for i, image_name in enumerate(uninfected_images):
         image = image.resize((SIZE, SIZE))
         dataset.append(np.array(image))
         label.append(1)
+
 #Apply CNN
 # ### Build the model
+
+#############################################################
+###2 conv and pool layers. with some normalization and drops in between.
+
+INPUT_SHAPE = (SIZE, SIZE, 3)   #change to (SIZE, SIZE, 3)
+inp = keras.layers.Input(shape=INPUT_SHAPE)
+
+conv1 = keras.layers.Conv2D(32, kernel_size=(3, 3), 
+                               activation='relu', padding='same')(inp)
+pool1 = keras.layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+norm1 = keras.layers.BatchNormalization(axis = -1)(pool1)
+drop1 = keras.layers.Dropout(rate=0.2)(norm1)
+conv2 = keras.layers.Conv2D(32, kernel_size=(3, 3), 
+                               activation='relu', padding='same')(drop1)
+pool2 = keras.layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+norm2 = keras.layers.BatchNormalization(axis = -1)(pool2)
+drop2 = keras.layers.Dropout(rate=0.2)(norm2)
+
+flat = keras.layers.Flatten()(drop2)  #Flatten the matrix to get it ready for dense.
+
+hidden1 = keras.layers.Dense(512, activation='relu')(flat)
+norm3 = keras.layers.BatchNormalization(axis = -1)(hidden1)
+drop3 = keras.layers.Dropout(rate=0.2)(norm3)
+hidden2 = keras.layers.Dense(256, activation='relu')(drop3)
+norm4 = keras.layers.BatchNormalization(axis = -1)(hidden2)
+drop4 = keras.layers.Dropout(rate=0.2)(norm4)
+
+out = keras.layers.Dense(2, activation='sigmoid')(drop4)   #units=1 gives error
+
+model = keras.Model(inputs=inp, outputs=out)
+model.compile(optimizer='adam',
+                loss='categorical_crossentropy',   #Check between binary_crossentropy and categorical_crossentropy
+                metrics=['accuracy'])
+print(model.summary())
+    
+    
+###############################################################    
