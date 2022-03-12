91587abf334df49fc7b9e399a10dad0ef35c7ba2
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2022-03-12 20:10:53 +0100

    import

--- Malaria/Malaria.py
+++ Malaria/Malaria.py
@@ -9,3 +9,9 @@ import numpy as np
 #Set the `numpy` pseudo-random generator at a fixed value
 #This helps with repeatable results everytime you run the code. 
 np.random.seed(1000)
+
+import matplotlib.pyplot as plt
+import os
+import cv2
+from PIL import Image
+import keras
