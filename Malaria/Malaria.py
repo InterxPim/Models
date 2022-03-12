1d6b8c64e8cf95ac3ac9be8ce195fac5e0627ada
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2022-03-12 21:44:42 +0100

    accuracy calculation with model saved

--- Malaria/Malaria.py
+++ Malaria/Malaria.py
@@ -125,3 +125,39 @@ history = model.fit(np.array(X_train),
                          shuffle = False
                       #   callbacks=callbacks
                      )
+
+# ## Accuracy calculation
+# 
+# I'll now calculate the accuracy on the test data.
+
+print("Test_Accuracy: {:.2f}%".format(model.evaluate(np.array(X_test), np.array(y_test))[1]*100))
+
+
+
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
+t = f.suptitle('CNN Performance', fontsize=12)
+f.subplots_adjust(top=0.85, wspace=0.3)
+
+max_epoch = len(history.history['accuracy'])+1
+epoch_list = list(range(1,max_epoch))
+ax1.plot(epoch_list, history.history['accuracy'], label='Train Accuracy')
+ax1.plot(epoch_list, history.history['val_accuracy'], label='Validation Accuracy')
+ax1.set_xticks(np.arange(1, max_epoch, 5))
+ax1.set_ylabel('Accuracy Value')
+ax1.set_xlabel('Epoch')
+ax1.set_title('Accuracy')
+l1 = ax1.legend(loc="best")
+
+ax2.plot(epoch_list, history.history['loss'], label='Train Loss')
+ax2.plot(epoch_list, history.history['val_loss'], label='Validation Loss')
+ax2.set_xticks(np.arange(1, max_epoch, 5))
+ax2.set_ylabel('Loss Value')
+ax2.set_xlabel('Epoch')
+ax2.set_title('Loss')
+l2 = ax2.legend(loc="best")
+
+
+#Save the model
+model.save('malaria_cnn.h5')
+
