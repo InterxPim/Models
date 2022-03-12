c167c7b7c27e896cafe8aa3b9db22dad48d1ddd9
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2022-03-12 20:10:01 +0100

    malaria

--- Malaria/Malaria.py
+++ Malaria/Malaria.py
@@ -5,3 +5,7 @@ Spyder Editor
 
 #!/usr/bin/env python
 import numpy as np
+
+#Set the `numpy` pseudo-random generator at a fixed value
+#This helps with repeatable results everytime you run the code. 
+np.random.seed(1000)
